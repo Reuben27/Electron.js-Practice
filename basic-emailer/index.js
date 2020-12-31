dc43238ba97e66c743b3a5679f92ed2892280ff0
Reuben27 <reuben.sd@iitgn.ac.in>
2020-12-31 21:13:45 +0530

    Updates III

--- basic-emailer/index.js
+++ basic-emailer/index.js
@@ -1,13 +1,6 @@
-console.log("Index.js file is up and running my buoi!");
+console.log("Index.js file is up and running!");
 
 const nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-    user: 'sherlockholmesbaker480@gmail.com',
-    pass: 'sher1234'
-    }
-});
 
 const submitted = document.getElementById('submitbtn');
 var email = '';
@@ -15,27 +8,35 @@ var subject = '';
 var body = '';
 
 submitted.addEventListener('click', function(){
+    event.preventDefault();
     email  = document.getElementById("email").value;
     subject  = document.getElementById("sub").value;
     body  = document.getElementById("bod").value;
-    console.log(typeof(email));
     console.log("Function running!");
 
     sendemails(email, subject, body);
 
+    setTimeout(function(){
+        location.reload();
+    }, 5000);
 });
 
-console.log("Yeah Yeah");
-
 function sendemails(email, subject, body){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+        user: 'sherlockholmesbaker480@gmail.com',
+        pass: 'sher1234'
+        }
+    });
+
     var mailOptions = {
         from: "sherlockholmesbaker480@gmail.com",
         to: email,
         subject: subject,
         text: body
     };
-    
-    console.log("Hemlo");
+
     console.log(mailOptions);
 
     transporter.sendMail(mailOptions, function(error, info){
